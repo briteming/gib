ca81b8d25234865f181804d7f3babac5ffb260dc
briteming <briteming@gmail.com>
2024-12-11 16:57:55 -0800

    Update environment.d.ts

--- environment.d.ts
+++ environment.d.ts
@@ -1,12 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 namespace NodeJS {
   interface ProcessEnv {
-    GITHUB_CLIENT_ID: Ov23liq0VjxWPMAVb8Ig;
-    GITHUB_CLIENT_SECRET: a-string;
-    AUTHOR_NAME: brite;
-    BLOG_TITLE: brite's blog;
-    BLOG_DESCRIPTION: it's brite's blog;
-    NEXT_PUBLIC_OWNER: briteming;
-    NEXT_PUBLIC_REPO: gib;
+    GITHUB_CLIENT_ID: string;
+    GITHUB_CLIENT_SECRET: string;
+    AUTHOR_NAME: string;
+    BLOG_TITLE: string;
+    BLOG_DESCRIPTION: string;
+    NEXT_PUBLIC_OWNER: string;
+    NEXT_PUBLIC_REPO: string;
   }
 }
